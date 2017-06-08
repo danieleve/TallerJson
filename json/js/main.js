1dc73997381d233086c17ed4bdc298e5d2c5689c
danieleve <daniel-eve1@hotmail.com>
2017-06-08 13:55:02 -0500

    taller json

---
+++ json/js/main.js
@@ -0,0 +1,30 @@
+var datos = '[{ "Name" : "Alfreds Futterkiste", "City" : "Berlin", "Country" : "Germany" }, { "Name" : "Berglunds snabbköp", "City" : "Luleå", "Country" : "Sweden" }, { "Name" : "Centro comercial Moctezuma", "City" : "México D.F.", "Country" : "Mexico" }, { "Name" : "Ernst Handel", "City" : "Graz", "Country" : "Austria" }, { "Name" : "FISSA Fabrica Inter. Salchichas S.A.", "City" : "Madrid", "Country" : "Spain" }, { "Name" : "Galería del gastrónomo", "City" : "Barcelona", "Country" : "Spain" }, { "Name" : "Island Trading", "City" : "Cowes", "Country" : "UK" }, { "Name" : "Königlich Essen", "City" : "Brandenburg", "Country" : "Germany" }, { "Name" : "Laughing Bacchus Wine Cellars", "City" : "Vancouver", "Country" : "Canada" }, { "Name" : "Magazzini Alimentari Riuniti", "City" : "Bergamo", "Country" : "Italy" }, { "Name" : "North/South", "City" : "London", "Country" : "UK" }, { "Name" : "Paris spécialités", "City" : "Paris", "Country" : "France" }, { "Name" : "Rattlesnake Canyon Grocery", "City" : "Albuquerque", "Country" : "USA" }, { "Name" : "Simons bistro", "City" : "København", "Country" : "Denmark" }, { "Name" : "The Big Cheese", "City" : "Portland", "Country" : "USA" }, { "Name" : "Vaffeljernet", "City" : "Århus", "Country" : "Denmark" }, { "Name" : "Wolski Zajazd", "City" : "Warszawa", "Country" : "Poland" }]';
+
+var table = document.getElementById("tabla");
+var obj = JSON.parse(datos);
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+    });
+}
+var ordenado = sortByKey(obj, 'Country');
+for(i=0 ;i<ordenado.length ; i++){
+    console.log(ordenado[i].Country);
+}
+
+
+for (var i=0; i<obj.length; i++){
+    var j=0;
+    console.log(obj[i].Country);
+    var row = table.insertRow(1);
+    for (var propiedad in obj[i]){
+        console.log(obj[i][propiedad]);
+        var cell1 = row.insertCell(j);
+        cell1.innerHTML = obj[i][propiedad];
+        j++;
+    }
+}
+
+
